a88acdb35d48fd05dbd070985822c3480dbc6bc2
Aleix Alcacer <aleixalcacer@gmail.com>
2022-02-02 18:21:12 +0100

    Improve classification plot

--- app.R
+++ app.R
@@ -9,29 +9,24 @@
 
 library(shiny)
 library(tidyverse)
-
+library(ggbump)
 
 # Redefine ggplot
 
-ggplot <- function(...) ggplot2::ggplot(...) + theme_minimal()
-
-compute_streak <- function(day) {
-    if (length(day) == 1) {
-        return(1)
-    }
-    streak <- 1
-    for (i in 2:length(day)) {
-        if (day[i-1] - 1 == day[i]) {
-            streak = streak + 1
-        } else {
-            break
-        }
-    }
-    return(streak)
-}
+ggplot <- function(...) ggplot2::ggplot(...) +
+    theme_minimal() +
+    theme(
+        legend.position="bottom",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+    ) +
+    labs(
+        caption = "Creat per @aleixalbo"
+    )
 
 
 data <- read_csv("results.csv")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -51,14 +46,15 @@ ui <- fluidPage(
                         "Usuaris",
                         choices = data %>% select(author),
                         multiple = T,
+                        selected = unlist(data %>% select(author)),
             ),
         ),
 
         # Show a plot of the results
         mainPanel(
             tabsetPanel(
+                tabPanel("Classificació", plotOutput("rankingPlot")),
                 tabPanel("Històric", plotOutput("resultsPlot")),
-                tabPanel("Classificació", tableOutput('classification')),
                 tabPanel("Distribucions", plotOutput("distributionPlot")),
             )
         )
@@ -75,18 +71,17 @@ server <- function(input, output, session) {
     output$resultsPlot <- renderPlot(
         {
             if (!is.null(input$authors)) {
+                p_data <- filter_data()
                 
-                plot <- ggplot(filter_data(), aes(x=day, y=score, group=author, color=author)) +
-                    geom_line() +
-                    geom_point() +
+                plot <- ggplot(p_data, aes(x=day, y=score, group=author, color=author)) +
+                    geom_line(size=2, alpha=0.2) +
+                    geom_point(size=4) +
                     scale_y_reverse("Intents", limits=c(7.5, 0.5), breaks = c(7:1), minor_breaks = NULL) +
                     scale_x_continuous("Dies", breaks = seq(input$range[1], input$range[2], by = 1), minor_breaks = F) +
                     labs(
                         colour = "Usuari",
-                    ) + 
-                    theme(
-                        legend.position="bottom",
                     )
+                
                 plot
             }
         }
@@ -96,32 +91,82 @@ server <- function(input, output, session) {
         {
             if (!is.null(input$authors)) {
                 plot <- ggplot(filter_data(), aes(x=author, y=score, color=author, fill=author)) +
-                    geom_violin(trim = FALSE, alpha=0.3) + 
+                    geom_violin(trim = FALSE, alpha=0.2) + 
                     geom_jitter(height = 0.05, width = 0.05) +
                     scale_y_reverse("Intents", limits=c(7.5, 0.5), breaks = 7:1, minor_breaks = NULL) +
                     scale_x_discrete("Usuari") +
                     labs(
                         fill = "Usuari",
                         color = "Usuari",
-                    ) + 
+                    ) +
                     theme(
-                        legend.position="bottom",
-                    )
+                        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                        )
+                
                 plot
             }        
         }
     )
     
-    output$classification <- renderTable(
-        {   
-            if (!is.null(input$authors)) {
-                filter_data() %>% group_by(author) %>%
-                    summarize(score=sum(score) / n(), streak = as.integer(compute_streak(day))) %>%
-                    mutate(rank = row_number(), .before = author) %>%
-                    rename_all(~c("Rànking", "Usuari", "Puntuació mitjana", "Ratxa de dies"))
-            }
-        },
-        width="100%"
+    
+    output$rankingPlot <- renderPlot(
+        {
+            if(!is.null(input$authors)) {
+                p_data <- filter_data() %>%
+                    group_by(author) %>%
+                    arrange(day) %>%
+                    mutate(score=cumsum(score) / row_number()) %>%
+                    ungroup() %>%
+                    complete(author, day) %>%
+                    group_by(author) %>%
+                    fill(score, .direction = "down") %>%
+                    drop_na() %>%
+                    ungroup() %>%
+                    arrange(desc(day)) %>%
+                    group_by(day) %>%
+                    arrange(score) %>%
+                    mutate(rnk=row_number()) %>%
+                    ungroup()
+            
+                end_rank <- input$range[2]
+                end_nameline <- end_rank + 2
+
+                plot <- ggplot(p_data, aes(x=day, y=rnk, group=author, color=author)) +
+                    geom_bump(size=2) +
+                    geom_point(data = . %>% group_by(author) %>% slice_min(day), size=4) +
+                    geom_segment(data = . %>% group_by(author) %>% slice_max(day),
+                                 aes(x=end_rank, xend=end_nameline, y = rnk, yend=rnk), size=2) +
+                    geom_segment(data = . %>% group_by(author) %>% slice_max(day),
+                                 aes(x=end_nameline, xend=end_nameline + score - 2, y = rnk, yend=rnk), size=2) +
+                    geom_vline(xintercept = end_nameline, size= 2, color = "white") +
+                    geom_text(data = . %>% filter(day == max(day)),
+                              aes(x = end_nameline, label = author), hjust = 1, vjust = -1.1) +
+                    geom_text(data = . %>% filter(day == max(day)),
+                              aes(x = end_nameline + score + - 2 + 0.1, label = round(score, 2)), hjust =0, nudge_y = 0) +
+                    scale_y_reverse("",
+                                    breaks = length(unique(unlist(p_data %>% select(author)))):1,
+                                    limits = c(length(unique(unlist(p_data %>% select(author)))) + 0.5, 0.5)
+                                    ) +
+                    scale_x_continuous("",
+                                       breaks = seq(input$range[1], input$range[2], by = 1),
+                                       limits = c(input$range[1], input$range[2] + 5),
+                                       ) +
+                    theme_void() +
+                    labs(
+                         # title = "WordleCAT",
+                         subtitle = str_wrap("Evolució al llarg dels dies de la classifciació dels usuaris de twitter que han compartit el resultats del #WordleCAT mencionant a @aleixalbo.", 100),
+                        ) + 
+                    theme(
+                        plot.margin = margin(.5, .5, .5, .5, "cm"),
+                        legend.position = "none",
+                        # axis.text.x = element_text(),
+                        plot.title = element_text(size = 16, family = "Helvetica", face = "bold"),
+                    )
+                
+                    
+                plot
+                }
+        }
     )
 }
 
